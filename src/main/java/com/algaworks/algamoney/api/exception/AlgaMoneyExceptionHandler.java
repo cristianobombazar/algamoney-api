d57ecefbed39f2481ed5cefa608f0f7e04b4a5c5
cristianobombazar <31328601+cristianobombazar@users.noreply.github.com>
2018-01-09 12:38:16 -0200

    3.9. Tratando erros com ExceptionHandler

--- src/main/java/com/algaworks/algamoney/api/exception/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exception/AlgaMoneyExceptionHandler.java
@@ -1,7 +1,55 @@
 package com.algaworks.algamoney.api.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Locale;
+
+@ControllerAdvice //observa a aplicação
 public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String messageUser      = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        String messageDeveloper = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Error(messageUser, messageDeveloper), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    class Error{
+
+        private String messageUser;
+        private String messageDeveloper;
+
+        public Error(String messageUser, String messageDeveloper) {
+            this.messageUser = messageUser;
+            this.messageDeveloper = messageDeveloper;
+        }
+
+        public String getMessageUser() {
+            return messageUser;
+        }
+
+        public void setMessageUser(String messageUser) {
+            this.messageUser = messageUser;
+        }
+
+        public String getMessageDeveloper() {
+            return messageDeveloper;
+        }
+
+        public void setMessageDeveloper(String messageDeveloper) {
+            this.messageDeveloper = messageDeveloper;
+        }
+    }
 }

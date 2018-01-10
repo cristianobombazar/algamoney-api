82f14bbbf6aad31c3b3944b163d7771f79ecdd64
cristianobombazar <31328601+cristianobombazar@users.noreply.github.com>
2018-01-10 14:56:51 -0200

    5.4. Validando inconsistências

--- src/main/java/com/algaworks/algamoney/api/exception/ExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exception/ExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.algaworks.algamoney.api.exception;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -57,6 +59,12 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, Arrays.asList(new Error(messageUser, messageDeveloper)), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
+    @org.springframework.web.bind.annotation.ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+        String messageUser      = messageSource.getMessage("recurso.operacao-nao-permitida",null, LocaleContextHolder.getLocale());
+        String messageDeveloper = ExceptionUtils.getRootCauseMessage(ex);
+        return handleExceptionInternal(ex, Arrays.asList(new Error(messageUser, messageDeveloper)), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
 
     class Error{
 

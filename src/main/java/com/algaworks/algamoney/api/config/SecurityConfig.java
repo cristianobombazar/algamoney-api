3efbc88591c668ddd347bd2b7aa3fbec086ded3c
cristianobombazar <31328601+cristianobombazar@users.noreply.github.com>
2018-01-12 11:45:30 -0200

    6.3. Implementando segurança com OAuth 2 e Password Flow

--- src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
@@ -1,36 +1,36 @@
-package com.algaworks.algamoney.api.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password("admin")
-            .roles("ROLES");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/categoria")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-            .httpBasic().and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .csrf().disable();
-
-    }
-}
+//package com.algaworks.algamoney.api.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//            .withUser("admin")
+//            .password("admin")
+//            .roles("ROLES");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/categoria") //libera apenas categoria
+//                .permitAll() //permite todas as requisições para categoria
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//            .httpBasic().and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//            .csrf().disable();
+//
+//    }
+//}

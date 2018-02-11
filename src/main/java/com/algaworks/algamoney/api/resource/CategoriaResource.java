4e625e4facb2bb28b7a1f222242ff073d7f019f3
cristianobombazar <31328601+cristianobombazar@users.noreply.github.com>
2018-02-11 18:19:24 -0200

    6.12. Adicionando permissões de acesso

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,13 +29,15 @@ public class CategoriaResource {
     private ApplicationEventPublisher publisher;
 
     @GetMapping
-    @CrossOrigin(maxAge = 10) //SO PODE FAZER REQUISIÇÃO PARA ESSE RECURSO A CADA 10 SEGUNDOS
+    //@CrossOrigin(maxAge = 10) //SO PODE FAZER REQUISIÇÃO PARA ESSE RECURSO A CADA 10 SEGUNDOS
+    @PreAuthorize("hasAnyAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasAnyScope('read')")
     public List<Categoria> findAll(){
         return repository.findAll();
     }
 
     @PostMapping
     //@ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasAnyScope('write')")
     public ResponseEntity<Categoria> save(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         categoria = repository.save(categoria);
         publisher.publishEvent(new ResourceEvent(this, response, categoria.getId()));
@@ -42,6 +45,7 @@ public class CategoriaResource {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasAnyScope('read')")
     public ResponseEntity<Categoria> find(@PathVariable Long id) {
         Categoria categoria = repository.findOne(id);
         return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
